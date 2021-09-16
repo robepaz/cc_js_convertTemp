d43b4b5ad196ad3810af9036d3c84640d7e8232f
Roberto Paz <fsjedi@outlook.com>
2021-09-16 08:49:29 -0400

    imprime k a f

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // fijamos constante kelvin
-const kelvin = 293;
+const kelvin = 293
 
 //convierte kelvin a celsius
-let celsius = kelvin - 273;
+let celsius = kelvin - 273
 
 // convierte celsius a farenheit
-let fahrenheit = celsius * (9/5) + 32;
+let fahrenheit = celsius * (9 / 5) + 32
 
 // quita decimales a f
-fahrenheit = Math.floor(fahrenheit);
+fahrenheit = Math.floor(fahrenheit)
